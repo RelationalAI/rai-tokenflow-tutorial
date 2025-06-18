19e8e39fe07ef1baef1d97c88b0b4b2ade756b5f
Alexandros Ntoulas <alex.ntoulas@relational.ai>
2025-06-18 19:00:57 +0300

    creating account & users

---
+++ setup/0_create_account.sql
@@ -0,0 +1,18 @@
+CREATE ACCOUNT TOKENFLOW_DEV
+    ADMIN_NAME = <EXISTING_USER_HERE>
+    ADMIN_PASSWORD = '<PASSWORD_HERE>'
+    EMAIL = '<EMAIL_HERE>'
+    MUST_CHANGE_PASSWORD = FALSE
+    EDITION = ENTERPRISE
+    ;
+
+--{
+--  "accountLocator": "ZGC45332",
+--  "accountLocatorUrl": "https://zgc45332.us-east-1.snowflakecomputing.com",
+--  "accountName": "TOKENFLOW_DEV",
+--  "url": "https://wcwhvmh-tokenflow_dev.snowflakecomputing.com",
+--  "edition": "ENTERPRISE",
+--  "regionGroup": "PUBLIC",
+--  "cloud": "AWS",
+--  "region": "AWS_US_EAST_1"
+--}
