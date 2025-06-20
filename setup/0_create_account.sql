e50fc9c5da37dee950028e653ac19baad1edcd4c
Alexandros Ntoulas <alex.ntoulas@relational.ai>
2025-06-20 13:09:50 +0300

    tweaks

--- setup/0_create_account.sql
+++ setup/0_create_account.sql
@@ -5,14 +5,3 @@ CREATE ACCOUNT TOKENFLOW_DEV
     MUST_CHANGE_PASSWORD = FALSE
     EDITION = ENTERPRISE
     ;
-
---{
---  "accountLocator": "ZGC45332",
---  "accountLocatorUrl": "https://zgc45332.us-east-1.snowflakecomputing.com",
---  "accountName": "TOKENFLOW_DEV",
---  "url": "https://wcwhvmh-tokenflow_dev.snowflakecomputing.com",
---  "edition": "ENTERPRISE",
---  "regionGroup": "PUBLIC",
---  "cloud": "AWS",
---  "region": "AWS_US_EAST_1"
---}
